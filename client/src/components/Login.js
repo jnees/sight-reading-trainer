40a06396d86f22d95ce6017bffb810eb5bf9ceec
jnees <joshua.nees@gmail.com>
2021-11-19 13:13:32 -0800

    Add state hooks took Login and Register forms.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container,
          FormGroup,
@@ -14,20 +14,50 @@ import { Container,
 -----------------------------------------------------*/
 
 export default function Login() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    // Have changes to the form by the user update the current state.
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+    
+    // OnSubmit of form -> Validate password fields and initiate action
+    const onSubmit = e => {
+        e.preventDefault();
+        
+        console.log("Success");
+    }
+
     return (
         <Container id="loginForm" >
             <h1 id="headline">Sight Reading Trainer</h1>
             <p>Get better at reading piano music.</p>
-            <Form>
+            <Form onSubmit={e => onSubmit(e)}>
                 <FormGroup>
-                <Label for="Email">
-                Email
-                </Label>
-                <Input id="Email" name="email" type="email"/>
+                    <Label for="Email">
+                    Email
+                    </Label>
+                    <Input 
+                        id="Email" 
+                        name="email" 
+                        type="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Label for="Password">Password</Label>
-                    <Input id="Password" name="password" type="password"/>
+                    <Input 
+                        id="Password" 
+                        name="password" 
+                        type="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
                 </FormGroup>
                 <Button color="primary" id="loginSubmitBtn">Login</Button>
             </Form>
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Container,
          FormGroup,
@@ -12,28 +12,73 @@ import { Container,
     This component is the registration page. Users
     are directed here if they are not authenticated.
 -----------------------------------------------------*/
+export default function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
 
-export default function Login() {
+    const { name, email, password, password2 } = formData;
+
+    // Have changes to the form by the user update the current state.
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+    
+    // OnSubmit of form -> Validate password fields and initiate action
+    const onSubmit = e => {
+        e.preventDefault();
+        if(password !== password2){
+            console.log("Passwords do not match.")
+        } else {
+            console.log('Success');
+        }
+    }
+
     return (
             <Container id="loginForm" >
-                <h1 id="headline">Registration</h1>
+                <h1 id="headline">Sign Up</h1>
                 <p>Sight Reading Trainer is completely free.</p>
-                <Form>
+                <Form onSubmit={e => onSubmit(e)}>
                     <FormGroup>
                         <Label for="name">
                         Username
                         </Label>
-                        <Input id="Name" name="name" type="text"/>
+                        <Input 
+                            id="Name" 
+                            name="name" 
+                            type="text" 
+                            value={name} 
+                            onChange={e => onChange(e)}/>
                         </FormGroup>
                     <FormGroup>
-                    <Label for="Email">
-                    Email
-                    </Label>
-                    <Input id="Email" name="email" type="email"/>
+                        <Label for="Email">
+                        Email
+                        </Label>
+                        <Input 
+                            id="Email" 
+                            name="email" 
+                            type="email" 
+                            value={email}
+                            onChange={e => onChange(e)}/>
                     </FormGroup>
                     <FormGroup>
                         <Label for="Password">Password (6 or more characters)</Label>
-                        <Input id="Password" name="password" type="password"/>
+                        <Input 
+                            id="Password" 
+                            name="password" 
+                            type="password" 
+                            value={password}
+                            onChange={e => onChange(e)}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="Password2">Type your password again.</Label>
+                        <Input 
+                            id="Password2" 
+                            name="password2" 
+                            type="password" 
+                            value={password2}
+                            onChange={e => onChange(e)}/>
                     </FormGroup>
                     <Button color="primary" id="registerSubmitBtn">Register</Button>
                 </Form>
