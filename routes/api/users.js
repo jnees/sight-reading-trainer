0c7164a6a7e3e342776349d49853eb21d63aad7f
jnees <joshua.nees@gmail.com>
2021-11-18 20:18:27 -0800

    Add JWT token creation and return on successful User register.

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,41 +4,49 @@ const { check, validationResult } = require('express-validator')
 const gravatar = require('gravatar');
 const User = require('../../models/User');
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // @route   Post api/users
-// @desc    Register user
+// @desc    Registers a new user. Registration requires
+//          a valid email address, a name, and a
+//          password with at least 6 characters.
 // @access  Public
-router.post('/', [
-    check('name', 'Name is required')
-        .not()
-        .isEmpty(),
-    check('email', "Please include a valid email").isEmail(),
-    check('password', "Please enter a password with 6 or more characters")
-        .isLength({min:6})
-], async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+router.post('/',
+    // Validate input from form with express-validator package.
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', "Please include a valid email").isEmail(),
+        check('password', "Password requires 6 or more characters").isLength({min:6})
+    ], 
+    async (req, res) => {
+
+        // Return all validation issues in an array if failed.
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
     }
 
     const { name, email, password } = req.body;
 
+    // Case -> Validation Passed. Try to Register User
     try {
-        // Check if user exists already
         let user = await User.findOne({ email });
 
+        // Case -> User already registered under this email (Invalid Register)
         if(user){
             return res.status(400).json({ errors: [{ msg: "User already exists" }] });
         }
 
-        // Get user's gravatar
+        // If the email is attached to a Gravatar, get the url for it. 
+        // Gravatar docs: https://en.gravatar.com/
         const avatar = gravatar.url(email, {
             s: "200",
             r: "pg",
             d: "mm"
         });
 
-        // New user
+        // Case -> User does not already exist (Add new user)
         user = new User({
             name,
             email,
@@ -46,14 +54,38 @@ router.post('/', [
             password
         })
 
-        // Encrypt password
+        // Encrypt the user's password for storage.
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-        // return jsonwebtoken
+        /*------------------------------------------------
+            Create a json web token for the user
+            and return it to the front end on a successful 
+            user registration. This will contain the user's
+            id number from the db encoded within it along
+            with other info.
 
-        return res.send("User registered");
+            For more info on encoding and decoding 
+            jwt: https://jwt.io/
+        --------------------------------------------------*/
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        // Create the JWT Token
+        jwt.sign(
+            payload, 
+            process.env.JWT_SECRET,
+            { expiresIn: "7 days"},
+            (err, token) => {
+                if(err) throw err;
+                return res.json({ token });
+            }
+        );
+
     } catch(err) {
         console.error(err.message);
         return res.status(500).send('Server Error');
