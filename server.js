86e6f980a8edce7f4a26feea2104fc39d998b11e
jnees <joshua.nees@gmail.com>
2021-12-14 14:58:13 -0800

    Add static for production

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const attempts = require("./routes/api/attempts");
 const stats = require("./routes/api/stats");
 const auth = require("./routes/api/auth");
 const users = require("./routes/api/users");
+const production = process.env.NODE_ENV.trim() === "production";
 require("dotenv").config();
 
 const app = express();
@@ -23,6 +24,15 @@ app.use("/api/attempts", attempts);
 app.use("/api/stats", stats);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+
+// Serve static if in production
+if(production){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // Listener
 const PORT = process.env.port || 3000;
