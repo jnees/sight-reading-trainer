d272ddebd5ec312e102ab411defc332f80be8840
jnees <joshua.nees@gmail.com>
2021-12-15 15:19:54 -0800

    update heroku settings.

--- server.js
+++ server.js
@@ -26,15 +26,15 @@ app.use("/api/users", users);
 app.use("/api/auth", auth);
 
 // Serve static if in production
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join('build', 'index.html'));
     });
 }
 
 // Listener
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 5000;
 app.listen(PORT, () =>{
     console.log("Server running on port " + PORT);
 })
